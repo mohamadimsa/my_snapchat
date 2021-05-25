b13920c0deb39bf2e996a83f9cabb5e3fecf22c9
mohamadi <mohamadi1.msa-abdou@epitech.eu>
2021-05-25 17:02:21 +0200

    a

--- screens/ConnectionScreen.js
+++ screens/ConnectionScreen.js
@@ -1,23 +1,35 @@
-import React from "react";
+import React,{useState} from "react";
 import {View, Text, Stylesheet,TextInput, Button} from "react-native" ;
 
 const ConnectionScreen = (props) => {
 
-    
+    const [Email, setEmail] = useState("");
+    const [Psw, setPsw] = useState("");
+    function test() {    
+       
+        console.log('Le lien a été cliqué.');
+    }
+   
         
     return(
     <View>
     <Text>Email:</Text>
-      <TextInput placeholder="entrez votre adresse email"/>
+      <TextInput placeholder="entrez votre adresse email"
+      onChangeText={(Email)=>setEmail(Email)}
+      />
       <Text>mot de passe:</Text>
-      <TextInput secureTextEntry={true} placeholder="entrez votre mot de passe" />
+      <TextInput secureTextEntry={true} placeholder="entrez votre mot de passe"
+       onChangeText={(Psw)=>setPsw(Psw)}
+
+      
+      />
       <Button
         title="Se connecter"
        
-        onPress={() => 
-            
-            
-            props.navigation.navigate("Home")
+        onPress={(e) => 
+          
+            test()
+  
         }
       />
     </View>
