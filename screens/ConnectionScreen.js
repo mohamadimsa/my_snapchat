83cf2fa43baa55c47c138bae25c458432bba1992
mohamadi <mohamadi1.msa-abdou@epitech.eu>
2021-05-25 15:27:10 +0200

    page login

--- screens/ConnectionScreen.js
+++ screens/ConnectionScreen.js
@@ -1,11 +1,27 @@
 import React from "react";
-import {View, Text, Stylesheet} from "react-native" ;
+import {View, Text, Stylesheet,TextInput, Button} from "react-native" ;
 
 const ConnectionScreen = (props) => {
+
+    
+        
     return(
-        <View>
-            <Text>register screen</Text>
-        </View>
+    <View>
+    <Text>Email:</Text>
+      <TextInput placeholder="entrez votre adresse email"/>
+      <Text>mot de passe:</Text>
+      <TextInput secureTextEntry={true} placeholder="entrez votre mot de passe" />
+      <Button
+        title="Se connecter"
+       
+        onPress={() => 
+            
+            
+            props.navigation.navigate("Home")
+        }
+      />
+    </View>
+
     )
 }
 
